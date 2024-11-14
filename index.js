cdf63efc0e9ff9f0d441c468c005d8119c5b2267
Jared Flores <jaredfm24@gmail.com>
2024-11-14 13:46:36 -0600

    3.3 Phonebook backend step 3

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ app.get('/api/contacts', (request, response) => {
   response.json(contacts);
 });
 
+app.get('/api/contacts/:id', (request, response) => {
+  const id = request.params.id;
+  const contact = contacts.find(contact => contact.id === id);
+
+  if (contact) {
+    response.json(contact);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
