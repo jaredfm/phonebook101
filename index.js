59aa55abdc6aa73ce823198be7ee77e452bd728c
Jared Flores <jaredfm24@gmail.com>
2024-11-20 10:25:40 -0600

    3.11 Full Stack Phonebook

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('data', (request, response) => JSON.stringify(request.body));
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 app.use(cors());
+app.use(express.static('dist'));
 
 let contacts = [
   {
