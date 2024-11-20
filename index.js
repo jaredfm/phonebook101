207b0d3efc298078a96cbb731e41513403449267
Jared Flores <jaredfm24@gmail.com>
2024-11-19 19:17:25 -0600

    3.9 Phonebook step 9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -7,6 +8,7 @@ morgan.token('data', (request, response) => JSON.stringify(request.body));
 
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
+app.use(cors());
 
 let contacts = [
   {
